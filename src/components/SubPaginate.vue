2ba880cc75264d4b23d921085671afd71ef93f76
falana sheriff <falanasheriffdeen@gmail.com>
2024-05-04 15:55:36 +0100

    creating a reusable pagination component

--- src/components/SubPaginate.vue
+++ src/components/SubPaginate.vue
@@ -1,7 +1,34 @@
 <template>
   <main>
     <h4>PAGINATION</h4>
+    <p>{{ totalResults }}</p>
+    <p>{{ perPage }}</p>
+    <p>{{ totalPages }}</p>
+    <p>{{ currentPage }}</p>
+    <p>{{ maxVisibleButtons }}</p>
   </main>
+  <ul>
+    <li>
+      <button type="button">first</button>
+    </li>
+    
+    <li>
+      <button type="button">prev</button>
+    </li>
+
+    <li v-for="page in pages" :key="page.name" >
+      <button class="num" type="button" :disabled="page.isDisabled">{{ page.name }}</button>
+    </li>
+
+    <li>
+      <button type="button">next</button>
+    </li>
+
+    <li>
+      <button type="button">last</button>
+    </li>
+
+  </ul>
 </template>
 
 <script>
@@ -11,6 +38,58 @@ export default {
         return {
 
         }
+    },
+    computed: {
+      startPage() {
+        // when on start page
+
+        if (this.currentPage === 1) {
+          return 1;
+        }
+
+        // when on the last page
+
+        if (this.currentPage === this.totalPages) {
+          return this.totalPages - this.maxVisibleButtons;
+        }
+
+        // when inbetween
+
+        return this.currentPage -1;
+      },
+
+      pages() {
+        const range = [];
+
+        for(let i = this.startPage; i <= Math.min(this.startPage + this.maxVisibleButtons -1, this.totalPages); i++) {
+          range.push({
+            name: i,
+            isDisabled: i === this.currentPage
+          });
+        }
+        return range;
+      }
+    },
+    props: {
+      totalResults: {
+        type: Number,
+        required: true
+      },
+      perPage : {
+        type: Number,
+        required: true
+      },
+      totalPages: {
+        type: Number,
+        required: true
+      },
+      currentPage: {
+        type: Number,
+        required: true
+      },
+      maxVisibleButtons: {
+        type: Number
+      }
     }
 
 }
@@ -22,5 +101,30 @@ main {
     height: 400px;
     width: 600px;
     margin: 50px auto;
+}
+
+p {
+  font-size: 35px;
+  color: purple;
+  line-height: 5px;
+}
+
+ul {
+  list-style-type: none;
+  border: 2px solid green;
+  width: 650px;
+  margin: 0px auto;
+}
+
+li {
+  display: inline-block;
+  margin: 0px 15px;
+}
+.num {
+  width: 30px;
+  height: 30px;
+  background: teal;
+  border-radius: 50%;
+  margin: 0px 5px;
 }
 </style>
--- src/views/Paginate.vue
+++ src/views/Paginate.vue
@@ -2,7 +2,11 @@
   <h1>TO PRACTICE PAGINATION</h1>
 
   <p>Will fetch the api from the parent component then pass down the details(i.e totalResults, perPage, totalPages, currentPage and maxVisibleButtons) as props to the child components. </p>
-  <SubPaginate />
+ 
+ 
+  <SubPaginate :totalResults="totalResults" :perPage="perPage" :totalPages="totalPages" :currentPage="currentPage" :maxVisibleButtons="maxVisibleButtons" />
+
+
   <h1>Players: {{ players }}</h1>
   <h1>totalResults: {{ players.length }}</h1>
   <h1>perPage: {{perPage }}</h1>
@@ -48,12 +52,54 @@ export default {
           jerseyNumber: 23,
           position: "midfielder"
         },
+        {
+          id: 6,
+          name: "petrovic",
+          jerseyNumber: 28,
+          position: "goalkeeper"
+        },
+        {
+          id: 7,
+          name: "gilchrist",
+          jerseyNumber: 23,
+          position: "defender"
+        },
+        {
+          id: 8,
+          name: "chalobah",
+          jerseyNumber: 14,
+          position: "defender"
+        },
+        {
+          id: 9,
+          name: "badiashile",
+          jerseyNumber: 5,
+          position: "defender"
+        },
+        {
+          id: 10,
+          name: "cucurella",
+          jerseyNumber: 3,
+          position: "defender"
+        },
+        {
+          id: 11,
+          name: "mudryk",
+          jerseyNumber: 10,
+          position: "midfielder"
+        },
+        {
+          id: 12,
+          name: "acheampong",
+          jerseyNumber: 43,
+          position: "defender"
+        }
       ];
-      const totalResults = 1;
-      const perPage = 2;
-      const totalPages = 4;
-      const currentPage = 6;
-      const maxVisibleButtons = 8;
+      const totalResults = players.length;
+      const perPage = 3;
+      const totalPages = players.length / perPage;
+      const currentPage = 1;
+      const maxVisibleButtons = 3;
 
       return {
         players,
