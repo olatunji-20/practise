3bd24f6974f76a39bd16e96d194e017b05db3a26
Falana Sheriff <Falanasheriffdeen@gmail.com>
2024-12-24 14:26:34 +0100

    Added toast for error in fetching data

--- src/stores/gojjo.js
+++ src/stores/gojjo.js
@@ -1,16 +1,23 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import axios from 'axios';
+import { h } from 'vue'
+import { useToast } from '@/components/ui/toast/use-toast'
+import { ToastAction } from '@/components/ui/toast'
+
+const { toast } = useToast()
 
 export const useGojjoStore = defineStore('gojjoStore', () => {
     const loading = ref(false);
-    const name = ref("sheriff");
+    const showError = ref(false);
+    const errMsg = ref("");
     const posts = ref([]);
 
     const getPosts = async () => {
         try {
             loading.value = true;
-            const res = await axios.get("https://sample-api.gojjoapps.com/api/posts/categories", {
+            const res = await axios.get("https://my-json-server.typicode.com/olatunji-20/delog-api/blog-posts", {
+            // const res = await axios.get("https://sample-api.gojjoapps.com/api/posts/categories", {
                 params: {
                     'api_token': 'Token 0a9a4277361b4a69da24211bfd76d853c696019e'
                 }
@@ -19,9 +26,24 @@ export const useGojjoStore = defineStore('gojjoStore', () => {
             loading.value = false
         }
         catch (err) {
-            console.log("ERROR")
+            loading.value = false;
+            showError.value = true;
+            errMsg.value = err + " " + "Sorry!!! The API could not be fetched.";
+            toast({
+                title: 'Uh oh! Something went wrong.',
+                description: 'There was a problem with your request.',
+                action: h(ToastAction, {
+                altText: 'Try again',
+                }, {
+                default: () => 'Try again',
+                }),
+            });
         }
     }
 
-    return {loading, name, posts, getPosts}
+    const createPost = () => {
+
+    }
+
+    return {loading, name, posts, getPosts, showError, errMsg}
 })
--- src/views/Gojjo.vue
+++ src/views/Gojjo.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="p-32">
-        <h1 class="text-blue-600">Gojjo</h1>
-        <p>{{ gojjoStore.name }}</p>
+    <div class="p-16">
+        <h1 class="text-blue-600 text-6xl font-bold mb-8">{{ 'Gojjo Test' }}</h1>
         <div v-if="storeItems.loading" class="p-16 border border-red-400">
             <p class="text-green-800 text-2xl">{{ 'Loading...' }}</p>
         </div>
-        <div v-else v-for="post in storeItems.posts" :key="post.id" class="my-4 p-16 border border-red-400">
-            <p>{{ post.productName }}</p>
-            <p>{{ post.price }}</p>
-            <p>{{ post.rating }}</p>
-            <p>{{ post.discount }}</p>
+        <div v-else v-for="post in storeItems.posts" :key="post.id" class="my-4 p-16 border border-red-600 rounded-lg">
+            <p class="text-4xl text-green-600">{{ post.authorName }}</p>
+            <p class="leading-10 text-blue-800 text-lg">{{ post.topic }}</p>
+            <p class="text-yellow-600 text-xs">{{ post.sneakPeak }}</p>
+        </div>
+        <div v-if="storeItems.showError" class="border-2 p-6">
+            <p class="">{{ storeItems.errMsg }}</p>
         </div>
     </div>
 </template>
@@ -18,11 +19,13 @@
 import { defineComponent } from 'vue';
 import { useGojjoStore } from '@/stores/gojjo';
 
-const gojjoStore = useGojjoStore();
-gojjoStore.getPosts()
-const storeItems = gojjoStore
-
 defineComponent({
     name: "Gojjo"
 });
+
+const gojjoStore = useGojjoStore();
+gojjoStore.getPosts();
+
+const storeItems = gojjoStore;
+
 </script>
